c21d3c73abadc01ef44db1b6fc91e7552767e266
h_ookubo <h_ookubo@japannext.net>
2023-10-11 16:44:54 +0900

    文字列中の最後の単語の、長さを出力

--- Length of Last Word.py
+++ Length of Last Word.py
@@ -2,8 +2,23 @@ from typing import *
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return 0
 
+        #末尾のスペースを削除
+        removeLastSpace = s.rstrip()
+
+        #スペースを基準にする
+        space = ' '
+        basedOnSpace = removeLastSpace.split(space)
+
+        #最後のスペースの後にある単語のみを代入する
+        lastWord = basedOnSpace[-1]
+        #print(lastWord)
+
+        #文字列の長さを取得する
+        num_char_end = len(lastWord)
+
+        #数値を引数に返す
+        return num_char_end
 
 print("スタート")
 assert Solution().lengthOfLastWord("Hello World") == 5
