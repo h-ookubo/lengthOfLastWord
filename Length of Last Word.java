22585780b81a4dab37ec6921eb741987ddfca3fb
h_ookubo <h_ookubo@japannext.net>
2023-10-13 12:55:21 +0900

    numCharLastWord = subtractCNCMCSfromPNCMCSの位置を修正

--- Length of Last Word.java
+++ Length of Last Word.java
@@ -1,24 +1,36 @@
 class Solution {
     public static void main(String[] args) {
       Solution ins = new Solution();
-      System.out.println(ins.lengthOfLastWord("Hello World") == 5);
+      System.out.println(ins.lengthOfLastWord("Hello   World") == 5);
       System.out.println(ins.lengthOfLastWord("   fly me   to   the moon  ") == 4);
       System.out.println(ins.lengthOfLastWord("luffy is still joyboy") == 6);
+      System.out.println(ins.lengthOfLastWord("good morning") == 7);
+      System.out.println(ins.lengthOfLastWord("good afternoon ") == 9);
+      System.out.println(ins.lengthOfLastWord("  good evening") == 7);
+      System.out.println(ins.lengthOfLastWord("Hello") == 5);
       System.out.println("すべてTrueなら一旦成功");
     }
 
 
     public int lengthOfLastWord(String s) {
       // ここにコードを書いてください
+      int test1;
+      char test2;
+      String test3 = s;
+      char test4 = test3.charAt(3);
+      int test5 = test3.length();
       for(int i = 0 ; i<10;i++){
       //ループ文のテンプレート
+      //System.out.println(test3);
+      //System.out.println(test4);
+      //System.out.println(test5);
       }
       
-      "abcde".charAt(3);  //"abcde"文字列の3+1文字目を取り出す
-      "abcde".length(); //文字列の長さを取得
+      //"abcde".charAt(3);  //"abcde"文字列の3+1文字目を取り出す
+      //"abcde".length(); //文字列の長さを取得
 
 
 
-      return 0;
+      return test5;
     }
 }
--- Length of Last Word.py
+++ Length of Last Word.py
@@ -26,7 +26,6 @@ class Solution:
 
         #sを一文字ずつiに代入して繰り返す
         for i in s :
-            numCharLastWord = subtractCNCMCSfromPNCMCS
             #iがa～zの文字もしくはスペースだった場合はtrue
             if i == 'a' or 'b' or 'c' or 'd' or 'e' or 'f' or 'g' or 'h' or 'i' or 'j' or 'k' or 'l' or 'm' or 'n' or 'o' or 'p' or 'q' or 'r' or 's' or 't' or 'u' or 'v' or 'w' or 'x' or 'y' or 'z' or ' ' :
                 count += 1
@@ -40,6 +39,7 @@ class Solution:
                     #numCharMinCountSpaceがpreviousNCMCSと同じでなかった場合はtrue
                     if numCharMinCountSpace != previousNCMCS:
                         subtractCNCMCSfromPNCMCS = numCharMinCountSpace - previousNCMCS
+                        numCharLastWord = subtractCNCMCSfromPNCMCS                        
                 #(1)
                 else:
                     numCharWord = count
@@ -50,11 +50,43 @@ class Solution:
 ##################################################
 
 print("スタート")
+print(Solution().lengthOfLastWord("ab "),"←2のはず")
+print(Solution().lengthOfLastWord("a "),"←1のはず")
+print(Solution().lengthOfLastWord("   fly me   to   the moon "),"←4のはず")
 assert Solution().lengthOfLastWord("   fly me   to   the moon  ") == 4
 assert Solution().lengthOfLastWord("Hello   World") == 5
 assert Solution().lengthOfLastWord("good morning") == 7
-assert Solution().lengthOfLastWord("good afternoon  ") == 9
+assert Solution().lengthOfLastWord("good afternoon ") == 9
 assert Solution().lengthOfLastWord("  good evening") == 7
 assert Solution().lengthOfLastWord("luffy is still joyboy") == 6
 assert Solution().lengthOfLastWord("Hello") == 5
 print("コンプリート!本番テストへ")
+
+"""
+        #文字数をカウントする変数
+        count = 0
+        #スペースを判定するための変数
+        space = ' '
+        #次のスペースまでをカウントする変数
+        countToSpace = 0
+        #一番最後に出てきた単語の文字数のみを代入する変数
+        numCharLastWord = 0
+        #スペースが入る手前までの文字数をカウントする変数
+        numCharWord = 0
+
+        #sを一文字ずつiに代入して繰り返す
+        for i in s :
+            #iがa～zの文字もしくはスペースだった場合はtrue
+            if i == 'a' or 'b' or 'c' or 'd' or 'e' or 'f' or 'g' or 'h' or 'i' or 'j' or 'k' or 'l' or 'm' or 'n' or 'o' or 'p' or 'q' or 'r' or 's' or 't' or 'u' or 'v' or 'w' or 'x' or 'y' or 'z' or ' ' :
+                count += 1
+                #iがspaceと同じだった場合はtrue
+                #それ以外はfalse、(1)へ
+                if i == space:
+                    countToSpace = count
+                #(1)
+                else:
+                    numCharWord = count
+                    numCharLastWord = numCharWord - countToSpace
+        #値を引数に返す
+        return numCharLastWord
+"""
